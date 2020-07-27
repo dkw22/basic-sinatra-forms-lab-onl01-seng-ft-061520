2bace4b197f3481a3e6e478364df79d341e09037
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-27 19:50:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
   get '/newteam' do
     erb :newteam
   end
@@ -17,3 +18,14 @@ class App < Sinatra::Base
   end
 
 end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
